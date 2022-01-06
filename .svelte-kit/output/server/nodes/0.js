1edcd63ad6fac604f38f01a2b8b6a4c4422063b8
phibr0 <bronzel.phillip@gmail.com>
2022-01-06 18:27:38 +0100

    Fix .gitignore

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/layout.svelte.js';
-export { module };
-export const entry = 'layout.svelte-2364333f.js';
-export const js = ["layout.svelte-2364333f.js","chunks/vendor-0fb0a8b3.js"];
-export const css = [];
-
